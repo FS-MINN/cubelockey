1a9ae6967d2062673294981740fad57fbb7d0dc6
Nakajyaga <nakajyaga.program.minn@gmail.com>
2022-02-24 18:14:39 +0900

    add: Racket

--- Assets/Script/MoveRacket2.cs
+++ Assets/Script/MoveRacket2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveRacket1 : MonoBehaviour
+public class MoveRacket2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,33 +13,33 @@ public class MoveRacket1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A) && transform.position.x > -40){
+        if(Input.GetKey(KeyCode.J) && transform.position.x > 20){
 	        transform.position = new Vector3(
             transform.position.x - 0.1f,
             transform.position.y,
             transform.position.z
             );
         }
-        if(Input.GetKey(KeyCode.D) && transform.position.x < -20){
+        if(Input.GetKey(KeyCode.L) && transform.position.x < 40){
         	transform.position = new Vector3(
             transform.position.x + 0.1f,
             transform.position.y,
             transform.position.z
             );
         }
-        if(Input.GetKey(KeyCode.W) && transform.position.z < 20){
+        if(Input.GetKey(KeyCode.I) && transform.position.z < 20){
         	transform.position = new Vector3(
             transform.position.x,
             transform.position.y,
             transform.position.z + 0.1f
             );
         }
-        if(Input.GetKey(KeyCode.S) && transform.position.z > -20){
+        if(Input.GetKey(KeyCode.K) && transform.position.z > -20){
         	transform.position = new Vector3(
             transform.position.x,
             transform.position.y,
             transform.position.z - 0.1f
             );
-        }
+        }   
     }
 }
